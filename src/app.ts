e81218d91e82f84f965e56f71bb5768ce8e919df
rova fitia <rovafiti@gmail.com>
2024-06-05 22:06:58 +0300

    feat: basic syntax

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,21 @@
-const compteur = document.querySelector('#compter')
+const a: string = "Hello world"
+const b: number = 3
+const c: boolean = true
+const d: string[] = ['Jao', 'koto', 'Joe']
+const e: number[] = [12, 15.5, 14]
+const f: any[] = ["Table", 12]  // any type : n'importe quoi 
+const g: any = "zezez"
+//Objet : le ?: veut dire optionnelle
+const users: {firstName: string, [key: string]: string} = {firstName: "Rakoto", lastName: "Jean"}
+const date: Date = new Date()
+const cb: Function = (e: MouseEvent):void => {
+
+}
+
+function prindId(id: number): void {
+
+}
+/* const compteur = document.querySelector('#compter')
 let i = 0 
 
 const increment = (e) => {
@@ -7,3 +24,4 @@ const increment = (e) => {
 }
 
 compteur.addEventListener('click', increment)
+*/
