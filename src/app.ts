5df113a6c9c0b0329cde11ffa04ae4410d67bfc1
rova fitia <rovafiti@gmail.com>
2024-07-12 23:13:08 +0300

    Feat: interface & class & readonly

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 /* -------------------------
   Alias: préciser le type d'un variable
 ---------------------------*/
-
+/*
 type User = { firstname: string; lastname: string };
 type DateString = string;
 type Id = string;
@@ -14,11 +14,12 @@ const users = {
 };
 
 type Users = typeof users;
+*/
 
 /* -------------------------
   Generics: préciser le type d'un retour d'une fonction
 ---------------------------*/
-
+/*
 function identity<ArgType>(a: ArgType): ArgType {
   return a;
 }
@@ -32,15 +33,19 @@ const bb = identity<string>("Hello");
 const cc = first(["hi", "hello"]);
 
 const arr: Array<string | number> = ["Koto", "Jean", 3];
+*/
 
 // Ici <Type extends { length: number }> est une contrainte : force le type du generic
+/*
 function consoleSize<Type extends { length: number }>(arg: Type): Type {
   console.log(arg.length);
   return arg;
 }
+*/
 /* -------------------------
   Syntaxe de base
 ---------------------------*/
+/*
 const a: string = "Hello world";
 const b: number = 3;
 const c: boolean = true;
@@ -54,16 +59,19 @@ const user: User = {
   firstname: "koto",
   lastname: "Kely",
 };
+*/
 /*
 const users: { firstName: string; [key: string]: string } = {
   firstName: "Rakoto",
   lastName: "Jean",
 };
 */
+/*
 const date: Date = new Date();
 const cb: Function = (e: MouseEvent): void => {};
 
 function prindId(id: number): void {}
+*/
 
 //const compteur = document.querySelector('#compter') as HTMLButtonElement
 // const compteur = <HTMLButtonElement>document.querySelector('#compter')
@@ -96,3 +104,142 @@ if(compteur) {
 
 // solution 2
 //compteur?.addEventListener("click", increment);
+
+/* ----------------
+  Readonly : permet de ne pas modifier les contenues
+  ----------------- */
+
+function reverse<T>(arr: readonly T[]): T[] {
+  return [...arr].reverse(); // Le spride opérator permet de créer un nouveau tableau
+}
+
+/* ----------------
+  Class
+  ----------------- */
+
+class A {
+  private a = 3;
+  protected b = "hello";
+
+  log() {
+    console.log(this.a);
+  }
+}
+
+class B extends A {
+  log() {
+    console.log(this.b);
+  }
+}
+
+const aInstance = new A();
+aInstance.log();
+
+class Collection<T> {
+  constructor(private items: T[]) {}
+
+  add(item: T): this {
+    this.items.push(item);
+    return this;
+  }
+
+  first(): T | null {
+    return this.items[0] || null;
+  }
+
+  isEqual(a: this) {
+    return a.items === this.items;
+  }
+}
+
+class CollectB<T> extends Collection<T> {}
+/*--------------------------
+  Change le this d'une class
+----------------------------*/
+class onSubscriber {
+  on(this: HTMLElement, name: string, cb: Function) {}
+}
+
+const collect = new Collection([1, 2]);
+collect.first();
+collect.add(15);
+
+const collectB = new CollectB(["hi", 2]);
+collectB.isEqual(collect);
+
+/*--------------------
+  DocTyping
+----------------------*/
+/*
+class Point {
+  x = 0;
+  y = 0;
+}
+
+class Geometry {
+  x = 0;
+  y = 0;
+  surface = 0;
+}
+
+function getX(p: Point) {
+  return p.x;
+}
+
+getX(new Geometry());
+*/
+
+/*---------------------
+  Class abstrait: c'est une modèle de class
+-----------------------*/
+
+abstract class Geometry {
+  x = 0;
+  y = 0;
+  abstract surface(): number;
+}
+
+class Triangle extends Geometry {
+  x = 2;
+  y = 2;
+
+  surface(): number {
+    return 3;
+  }
+}
+
+/* ----------------------
+  Class static
+-------------------------*/
+/*
+class Trigo {
+  static origin = { x: 0, y: 0 };
+}
+
+Trigo.origin;
+*/
+
+class Trigo {
+  static #origin: { x: number; y: number };
+
+  static {
+    Trigo.#origin = { x: 0, y: 0 };
+  }
+}
+
+/* --------------------
+  Interface
+-----------------------*/
+interface Point {
+  x: number;
+  y: number;
+}
+
+class TwoDimensionPoint implements Point {
+  x = 0;
+  y = 0;
+}
+
+function draw(p: Point) {}
+
+draw(new TwoDimensionPoint());
